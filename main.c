dd8fd85548027f7a87c4d785a523212b5e0913ab
jakovwf <jakovwf@gmail.com>
2026-01-28 20:12:31 +0100

    Kraj

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main() {
+    int fd[2];
+    pipe(fd);
+
+    if (fork() == 0) {
+        // CHILD
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[1]);
+
+        execl("/bin/df", "df", "-B1", NULL);
+        perror("execl");
+        exit(1);
+    }
+
+    // PARENT
+    close(fd[1]);
+
+    FILE *stream = fdopen(fd[0], "r");
+    char line[512];
+    unsigned long long total_free = 0;
+
+    // preskačemo header
+    fgets(line, sizeof(line), stream);
+
+    while (fgets(line, sizeof(line), stream)) {
+        char fs[128], mount[128];
+        unsigned long long size, used, avail;
+        int usep;
+
+        sscanf(line, "%s %llu %llu %llu %d%% %s",
+               fs, &size, &used, &avail, &usep, mount);
+
+        total_free += avail;
+    }
+
+    fclose(stream);
+    wait(NULL);
+
+    // ispis u GB
+    printf("Ukupno slobodno: %.2f GB\n",
+           total_free / (1024.0 * 1024 * 1024));
+
+    return 0;
+}
